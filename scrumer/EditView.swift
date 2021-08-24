804b967290f9f00d8340d688c5541668d7a7bc29
Saba Khutsishvili <sabakhuts@gmail.com>
2021-08-24 22:40:31 +0400

    Add scrum details in `EditView`

--- scrumer/EditView.swift
+++ scrumer/EditView.swift
@@ -9,16 +9,37 @@ import SwiftUI
 
 struct EditView: View {
     
-    @State private var scrumData = DailyScrum.Data()
+    @State private var data = DailyScrum.Data()
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $data.title)
+                
+                HStack {
+                    Slider(
+                        value: $data.lengthInMinutes,
+                        in: 5 ... 30,
+                        step: 1.0,
+                        label: { Text("Length") }
+                    )
+                    
+                    Spacer()
+                    
+                    Text("\(Int(data.lengthInMinutes)) minutes")
+                }
+                
+                ColorPicker("Color", selection: $data.color)
+            }
+        }
     }
 }
 
 struct EditView_Previews: PreviewProvider {
     
     static var previews: some View {
-        EditView()
+        NavigationView {
+            EditView()
+        }
     }
 }
