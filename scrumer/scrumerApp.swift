67eb4badc54a88bdf46349ad60ecf512f7f065cc
Saba Khutsishvili <sabakhuts@gmail.com>
2021-08-24 10:27:48 +0400

    Initial Commit

---
+++ scrumer/scrumerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumerApp.swift
+//  scrumer
+//
+//  Created by Saba Khutsishvili on 24.08.21.
+//
+
+import SwiftUI
+
+@main
+struct scrumerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
