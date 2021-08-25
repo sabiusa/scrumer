0773014964584b1b3df5a82baa67c1a6fdc3cf13
Saba Khutsishvili <sabakhuts@gmail.com>
2021-08-25 10:28:07 +0400

    Extract out `MeetingHeaderView`

---
+++ scrumer/Views/MeetingHeaderView.swift
@@ -0,0 +1,61 @@
+//
+//  MeetingHeaderView.swift
+//  scrumer
+//
+//  Created by Saba Khutsishvili on 25.08.21.
+//
+
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    
+    let secondsElapsed: Int
+    let secondsRemaining: Int
+    
+    private var progress: Double {
+        guard secondsRemaining > 0 else { return 1 }
+        let totalSeconds = Double(secondsElapsed + secondsRemaining)
+        return Double(secondsElapsed) / totalSeconds
+    }
+    
+    private var remainingMinutes: Int {
+        return secondsRemaining / 60
+    }
+    
+    private var remainingMinutesMetric: String {
+        return remainingMinutes == 1 ? "minute" : "minutes"
+    }
+    
+    var body: some View {
+        VStack {
+            ProgressView(value: progress)
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("\(secondsElapsed)", systemImage: "hourglass.bottomhalf.fill")
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel(Text("Time remaining"))
+        .accessibilityValue(Text("\(remainingMinutes) \(remainingMinutesMetric)"))
+    }
+}
+
+struct MeetingHeaderView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180)
+            .previewLayout(.sizeThatFits)
+    }
+}
