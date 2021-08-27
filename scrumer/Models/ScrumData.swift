502148e65928d420b5302d59151029eee45076bf
Saba Khutsishvili <sabakhuts@gmail.com>
2021-08-27 22:25:49 +0400

    Add saving data to persistent local store

--- scrumer/Models/ScrumData.swift
+++ scrumer/Models/ScrumData.swift
@@ -48,4 +48,20 @@ class ScrumData: ObservableObject {
             }
         }
     }
+    
+    func save() {
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let scrums = self?.scrums
+            else { fatalError("Self out of scope") }
+            
+            guard let data = try? JSONEncoder().encode(scrums)
+            else { fatalError("Error while encoding scrums") }
+            
+            do {
+                try data.write(to: Self.fileURL)
+            } catch {
+                fatalError("Can't write to file")
+            }
+        }
+    }
 }
