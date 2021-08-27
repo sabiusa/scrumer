c516948e68b5e61b86c1c2ece641d9d8287065f9
Saba Khutsishvili <sabakhuts@gmail.com>
2021-08-27 22:22:35 +0400

    Add loading data from persistent local storage

---
+++ scrumer/Models/ScrumData.swift
@@ -0,0 +1,51 @@
+//
+//  ScrumData.swift
+//  scrumer
+//
+//  Created by Saba Khutsishvili on 27.08.21.
+//
+
+import Foundation
+
+class ScrumData: ObservableObject {
+    
+    @Published var scrums = [DailyScrum]()
+    
+    private static var documentsFolderURL: URL {
+        do {
+            return try FileManager.default.url(
+                for: .documentDirectory,
+                in: .userDomainMask,
+                appropriateFor: nil,
+                create: false
+            )
+        } catch {
+            fatalError("No document directory")
+        }
+    }
+    private static var fileURL: URL {
+        return documentsFolderURL.appendingPathComponent("scrums.data")
+    }
+    
+    func load() {
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let data = try? Data(contentsOf: Self.fileURL)
+            else {
+                #if DEBUG
+                DispatchQueue.main.async {
+                    self?.scrums = DailyScrum.data
+                }
+                #endif
+                
+                return
+            }
+            
+            guard let dailyScrums = try? JSONDecoder().decode([DailyScrum].self, from: data)
+            else { fatalError("Can't decode saved scrum data") }
+            
+            DispatchQueue.main.async {
+                self?.scrums = dailyScrums
+            }
+        }
+    }
+}
